54e93d2fba8c5885649213c45cf077364c8c399e
Code-by-krishna <brothersrollhut@gmail.com>
2024-06-02 06:27:09 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Krishna from "./Krishna";
+
+
+
+function App() {
+  return (
+    <>
+    <h1>Coding with Amit</h1>
+    <Krishna />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Krishna.js
@@ -0,0 +1,10 @@
+function Krishna() {
+    return (
+        <>
+        <h1>This is a simpe Create react file.</h1>
+        <p>So we are discuss about the React js.</p>
+        </>
+
+    )
+}
+export default Krishna;
